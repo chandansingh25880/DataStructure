bc1d90a8eb3f813838ec84434b87af96da5deb67
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-13 19:19:50 +0530

    Added: Welcome to the LinkedList of different operation

---
+++ src/main/java/com/datastructure/MyNode.java
@@ -0,0 +1,9 @@
+package com.datastructure;
+
+public class MyNode
+{
+    public static void main(String[] args) {
+
+        System.out.println("Creating LinkedList for different operaton");
+    }
+}
---
+++ src/test/java/LinkedListTest.java
@@ -0,0 +1,9 @@
+import com.datastructure.MyNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTest {
+
+    }
+
+}
